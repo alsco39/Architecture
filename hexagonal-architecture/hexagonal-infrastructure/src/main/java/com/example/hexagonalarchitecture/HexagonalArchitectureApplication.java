5e7eb8c852d83ea8306ad25cd2721bb372ec5e89
min56 <minchaeg85@gmail.com>
2022-12-28 23:26:12 +0900

    [ ADD ] 프로젝트 세팅

---
+++ hexagonal-architecture/hexagonal-infrastructure/src/main/java/com/example/hexagonalarchitecture/HexagonalArchitectureApplication.java
@@ -0,0 +1,7 @@
+package com.example.hexagonalarchitecture;
+
+public class HexagonalArchitectureApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
